91e13d9de1ac3a559e12db934f696db5bdd73d5f
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-02-19 21:27:32 -0600

    Update Client.php

--- src/core/SQL/Client.php
+++ src/core/SQL/Client.php
@@ -1,1 +1,16 @@
 
+<?php
+namespace core\SQL;
+class Client{
+public static function addConexion(){
+  return new \mysqli("ip","usuario","pswd","db");
+}
+public static function stopConexion(){
+  return self::addConexion()->close();
+}
+
+
+}
+
+
+ ?>
