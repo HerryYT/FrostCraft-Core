2bf95d83a2c28c565e076b5d10ef22f7fb42a1e7
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-02-19 21:17:50 -0600

    Update Auth.php

--- src/core/Auth/Auth.php
+++ src/core/Auth/Auth.php
@@ -1,1 +1,32 @@
 
+<?php
+namespace core\Auth;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
+use pocketmine\{Server,Player};
+use core\DataBase;
+class Auth extends PluginTask{
+public $time = 20;
+public function __construct(DataBase $base){
+  parent::__construct($base);
+  $this->base = $base;
+}
+public function onRun($currentTick){
+  $base = $this->base;
+  if($this->time >0){$this->time--;}
+  if($this->time == 0){
+    foreach($base->getServer()->getOnlinePlayers() as $p){
+$base->getAuthMessage($p);
+$this->time = 20;
+}
+  }
+  foreach($base->getServer()->getOnlinePlayers() as $p){
+$base->getAuthpopup($p);
+  }
+}
+}
+
+
+ ?>
