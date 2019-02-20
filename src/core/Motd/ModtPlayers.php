b7720f0a069e3d51ed95c2d2613e0f63a9777231
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-02-19 21:22:56 -0600

    Update ModtPlayers.php

--- src/core/Motd/ModtPlayers.php
+++ src/core/Motd/ModtPlayers.php
@@ -1,1 +1,24 @@
 
+<?php
+namespace core\Motd;
+use pocketmine\event\Listener;
+use pocketmine\{Server,Player};
+use core\DataBase;
+use pocketmine\event\server\QueryRegenerateEvent;
+class ModtPlayers implements Listener{
+  public function __construct(DataBase $plugin){
+
+  $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+  $this->db = $plugin;
+  }
+
+  public function onQueryRegenerate(QueryRegenerateEvent $event){
+  $event->setPlayerCount(count($this->db->getServer()->getOnlinePlayers())+10);
+  $event->setMaxPlayerCount(8190);
+  }
+
+
+
+
+
+}
