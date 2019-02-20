d5305815da843842cf1b64751121ec12802502ca
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-02-19 21:24:00 -0600

    Update UpdateMotd.php

--- src/core/Motd/UpdateMotd.php
+++ src/core/Motd/UpdateMotd.php
@@ -1,1 +1,25 @@
 
+<?php
+namespace core\Motd;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
+use pocketmine\{Server,Player};
+use core\DataBase;
+class UpdateMotd extends PluginTask{
+  public $time = 5;
+public function __construct(DataBase $base){
+  parent::__construct($base);
+  $this->base = $base;
+}
+public function onRun($currentTick){
+  if($this->time>0){$this->time--;}
+  $prefix = "NextCraft : ";
+  $names = array("SkyWars","MobEvolution","Msg3","msg4");
+  if($this->time == 0){
+    $this->base->getServer()->getNetwork()->setName($prefix.$names[array_rand($names)]);
+    $this->time = 5;
+  }
+}
+}
