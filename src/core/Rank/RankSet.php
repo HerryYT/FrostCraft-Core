dd0403dcb78b6df7c4c3c05fee5dd34860ef7aa3
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-02-19 21:26:26 -0600

    Update RankSet.php

--- src/core/Rank/RankSet.php
+++ src/core/Rank/RankSet.php
@@ -1,1 +1,26 @@
 
+<?php
+namespace core\Rank;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
+use pocketmine\{Server,Player};
+use pocketmine\item\Item;
+use core\DataBase;
+class RankSet extends PluginTask{
+public function __construct(DataBase $base){
+  parent::__construct($base);
+  $this->base = $base;
+}
+public function onRun($currentTick){
+  $this->base->getBossMessage();
+foreach($this->base->getServer()->getlevelByName("world")->getPlayers() as $p){
+  $p->setFood(20);
+  $p->setHealth(20);
+  $this->base->LoggerAuth($p);
+}
+
+
+}
+}
